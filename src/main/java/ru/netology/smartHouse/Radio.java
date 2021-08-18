4e579b815cc7ae84f84a17cacd15821293873310
Elizaveta Shkryabiy <evshkryabiy@gmail.com>
2021-08-18 15:59:27 +0400

    code changed

--- src/main/java/ru/netology/smartHouse/Radio.java
+++ src/main/java/ru/netology/smartHouse/Radio.java
@@ -5,10 +5,12 @@ public class Radio {
     private String station;
     private int maxStation = 9;
     private int minStation = 1;
-    private int currentStation = 7;
+    private int currentStation;
     private String volume;
     private int maxVolume = 10;
     private int minVolume = 0;
+    private int currentVolume;
+    private int[] stations = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
 
     public int getCurrentStation() {
@@ -31,35 +33,25 @@ public class Radio {
     public int nextStation() {
         if (currentStation < maxStation) {
             currentStation = currentStation + 1;
+        } else {
+            currentStation = minStation;
         }
         return currentStation;
     }
 
-    public int nextStationWhenCurrentIsMax () {
-        if (currentStation == maxStation) {
-            this.currentStation = minStation;
-        }
-        return this.currentStation;
-    }
 
     public int prevStation() {
         if (currentStation > minStation) {
             currentStation = currentStation - 1;
+
+        } else {
+            currentStation = maxStation;
         }
         return currentStation;
     }
 
-    public int prevStationWhenCurrentIsMin() {
-
-        if (currentStation == minStation) {
-            this.currentStation = maxStation;
-        }
-        return this.currentStation;
-    }
-
     // Volume unit
 
-    private int currentVolume = 5;
 
     public int getCurrentVolume() {
         return currentVolume;
@@ -79,14 +71,8 @@ public class Radio {
     public int increaseVolume() {
         if (currentVolume < maxVolume) {
             currentVolume = currentVolume + 1;
-        }
-        return this.currentVolume;
-    }
-
-    public int increaseVolumeWhenCurrentIsMax() {
-
-        if (currentVolume == maxVolume) {
-            this.currentVolume = maxVolume;
+        } else {
+            currentVolume = maxVolume;
         }
         return this.currentVolume;
     }
@@ -95,16 +81,10 @@ public class Radio {
 
         if (currentVolume > minVolume) {
             currentVolume = currentVolume - 1;
+        }else{
+            currentVolume = minVolume;
         }
         return currentVolume;
-    }
-
-    public int decreaseVolumeWhenCurrentIsMin() {
-
-        if (currentVolume == minVolume) {
-            this.currentVolume = minVolume;
-        }
-        return this.currentVolume;
     }
 }
 
--- src/test/java/ru/netology/smartHouse/RadioTests.java
+++ src/test/java/ru/netology/smartHouse/RadioTests.java
@@ -3,8 +3,7 @@ package ru.netology.smartHouse;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class RadioTests {
 
@@ -20,6 +19,7 @@ public class RadioTests {
     @Test
     public void shouldCheckThatMoreThenMaxCurrentStationIsNotOkToSet() {
         Radio station = new Radio();
+        station.setCheckCorrectCurrentStation(7);
         station.setCheckCorrectCurrentStation(10);
         int actual = station.getCurrentStation();
         assertEquals(7, actual);
@@ -28,6 +28,7 @@ public class RadioTests {
     @Test
     public void shouldCheckThatLessThenMinCurrentStationIsNotOkToSet() {
         Radio station = new Radio();
+        station.setCheckCorrectCurrentStation(7);
         station.setCheckCorrectCurrentStation(0);
         int actual = station.getCurrentStation();
         assertEquals(7, actual);
@@ -36,38 +37,26 @@ public class RadioTests {
     @Test
     public void shouldGetCurrentStation() {
         Radio station = new Radio();
+        station.setCheckCorrectCurrentStation(7);
         int actual = station.getCurrentStation();
         assertEquals(7, actual);
     }
 
-    @Test
-    public void shouldSetCurrentStationLessThenMin() {
-        Radio station = new Radio();
-        station.setCheckCorrectCurrentStation(0);
-        int actual = station.getCurrentStation();
-        assertEquals(7, actual);
-    }
-
-    @Test
-    public void shouldSetCurrentStationMoreThenMax() {
-        Radio station = new Radio();
-        station.setCheckCorrectCurrentStation(11);
-        int actual = station.getCurrentStation();
-        assertEquals(7, actual);
-    }
 
     @Test
     public void shouldChangeToNextStation() {
         Radio station = new Radio();
+        station.setCheckCorrectCurrentStation(7);
         int actual = station.nextStation();
         assertEquals(8, actual);
+
     }
 
     @Test
     public void shouldChangeToNextStationWhenCurrentIsMax() {
         Radio station = new Radio();
         station.setCheckCorrectCurrentStation(9);
-        int actual = station.nextStationWhenCurrentIsMax();
+        int actual = station.nextStation();
         assertEquals(1, actual);
     }
 
@@ -75,6 +64,7 @@ public class RadioTests {
     @Test
     public void shouldChangeToPrevStation() {
         Radio station = new Radio();
+        station.setCheckCorrectCurrentStation(7);
         int actual = station.prevStation();
         assertEquals(6, actual);
     }
@@ -83,7 +73,7 @@ public class RadioTests {
     public void shouldChangeToPrevStationWhenCurrentIsMin() {
         Radio station = new Radio();
         station.setCheckCorrectCurrentStation(1);
-        int actual = station.prevStationWhenCurrentIsMin();
+        int actual = station.prevStation();
         assertEquals(9, actual);
     }
 
@@ -91,6 +81,7 @@ public class RadioTests {
     @Test
     public void shouldGetCurrentVolume() {
         Radio volume = new Radio();
+        volume.setCheckCorrectCurrentVolume(5);
         int actual = volume.getCurrentVolume();
         assertEquals(5, actual);
     }
@@ -107,6 +98,7 @@ public class RadioTests {
     @Test
     public void shouldNotSetCurrentVolumeMoreThenMax() {
         Radio volume = new Radio();
+        volume.setCheckCorrectCurrentVolume(5);
         volume.setCheckCorrectCurrentVolume(12);
         int actual = volume.getCurrentVolume();
         assertEquals(5, actual);
@@ -115,6 +107,7 @@ public class RadioTests {
     @Test
     public void shouldNotSetCurrentVolumeLessThenMin() {
         Radio volume = new Radio();
+        volume.setCheckCorrectCurrentVolume(5);
         volume.setCheckCorrectCurrentVolume(-1);
         int actual = volume.getCurrentVolume();
         assertEquals(5, actual);
@@ -123,6 +116,7 @@ public class RadioTests {
     @Test
     public void shouldIncreaseVolume() {
         Radio volume = new Radio();
+        volume.setCheckCorrectCurrentVolume(5);
         int actual = volume.increaseVolume();
         assertEquals(6, actual);
     }
@@ -130,14 +124,15 @@ public class RadioTests {
     @Test
     public void shouldIncreaseVolumeWhenCurrentIsMax() {
         Radio volume = new Radio();
-        volume.setCheckCorrectCurrentVolume(10) ;
-        int actual = volume.increaseVolumeWhenCurrentIsMax();
+        volume.setCheckCorrectCurrentVolume(10);
+        int actual = volume.increaseVolume();
         assertEquals(10, actual);
     }
 
     @Test
     public void shouldDecreaseVolume() {
         Radio volume = new Radio();
+        volume.setCheckCorrectCurrentVolume(5);
         int actual = volume.decreaseVolume();
         assertEquals(4, actual);
     }
@@ -145,8 +140,8 @@ public class RadioTests {
     @Test
     public void shouldDecreaseVolumeWhenCurrentIsMin() {
         Radio volume = new Radio();
-        volume.setCheckCorrectCurrentVolume(0) ;
-        int actual = volume.decreaseVolumeWhenCurrentIsMin();
+        volume.setCheckCorrectCurrentVolume(0);
+        int actual = volume.decreaseVolume();
         assertEquals(0, actual);
     }
 
